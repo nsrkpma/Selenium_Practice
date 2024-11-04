24fdd2c73de91d245c41a8780e6bde7421801d7f
Nirmal <nsr.kpma@gmail.com>
2024-11-04 17:44:25 +0530

    added assignment

--- src/main/java/Kane/Selenium_java/iframespractice.java
+++ src/main/java/Kane/Selenium_java/iframespractice.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class iframespractive {
+public class iframespractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,3 +22,4 @@ public class iframespractive {
 	}
 
 }
+	
